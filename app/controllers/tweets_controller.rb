5430aaed419a532f221d831b2a90310d35c39d8e
Shreya Shirke <shreyashirke29@gmail.com>
2024-04-23 13:12:00 +0530

    Added search

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,14 +1,15 @@
 class TweetsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!
+  before_action :set_tweet, only: [:edit, :update, :destroy]
 
   def index
     if user_signed_in?
       following_ids = current_user.followings.pluck(:id)
-      @tweets = Tweet.where(user_id: following_ids).order(created_at: :desc)
+      @tweets = Tweet.where(user_id: [current_user.id] + following_ids).order(created_at: :desc)
+      @tweet = Tweet.new
     else
       redirect_to new_user_session_path
     end
-    @tweet = Tweet.new
   end
 
   def new
@@ -31,13 +32,38 @@ class TweetsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @tweet.update(tweet_params)
+        format.html { redirect_to @tweet, notice: 'Tweet was successfully updated.' }
+        format.json { render :show, status: :ok, location: @tweet }
+        format.js
+      else
+        format.html { render :edit }
+        format.json { render json: @tweet.errors, status: :unprocessable_entity }
+        format.js
+      end
+    end
+  end
+
+  def destroy
+    @tweet.destroy
+    respond_to do |format|
+      format.html { redirect_to tweets_url, notice: 'Tweet was successfully destroyed.' }
+      format.json { head :no_content }
+      format.js
+    end
+  end
+
   private
+    def set_tweet
+      @tweet = Tweet.find(params[:id])
+    end
 
-  def set_tweet
-    @tweet = Tweet.find(params[:id])
-  end
-
-  def tweet_params
-    params.require(:tweet).permit(:content, :image, :video)
-  end
+    def tweet_params
+      params.require(:tweet).permit(:content, :image, :video)
+    end
 end
